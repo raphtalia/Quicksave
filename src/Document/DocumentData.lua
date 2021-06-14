84064b53a3028b18c9bb87659e259ac44a7ee74d
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-06-14 01:44:05 -0500

    Separated `copyDeep()`

--- src/Document/DocumentData.lua
+++ src/Document/DocumentData.lua
@@ -1,22 +1,7 @@
-local function copyDeep(dictionary)
-	-- https://github.com/Reselim/Quicksave/commit/b703fc4928c99927409f78900e62b20809b1a06a
-	local new = {}
-
-	for key, value in pairs(dictionary) do
-		if type(value) == "table" then
-			new[key] = copyDeep(value)
-		else
-			new[key] = value
-		end
-	end
-
-	return new
-end
+local copyDeep = require(script.Parent.copyDeep)
 
 local DocumentData = {}
 DocumentData.__index = DocumentData
-
--- TODO: Backups
 
 function DocumentData.new(options)
 	return setmetatable({
