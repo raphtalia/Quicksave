5c92309550cac4b61257dc57eb5f03a04b7a479b
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-09-13 19:20:34 -0500

    Update project structure

--- testRunner/runTests.server.lua
+++ testRunner/runTests.server.lua
@@ -1,6 +1,5 @@
-local RunService = game:GetService("RunService")
 local ServerStorage = game:GetService("ServerStorage")
 
-require(RunService.TestEZ).TestBootstrap:run({
+require(ServerStorage.TestEZ).TestBootstrap:run({
 	ServerStorage.Quicksave
 })
